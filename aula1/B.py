e2ce4a17cf704f8966bfcde69a1e687018c1de2a
bobasaurro <48527512+bobasaurro@users.noreply.github.com>
2019-04-03 14:50:33 -0300

    Add files via upload

---
+++ aula1/B.py
@@ -0,0 +1,4 @@
+variavel=int(input("digite um numero"))
+nome=int(input("digite outro numero"))
+soma= variavel+nome
+print(soma)
---
+++ aula1/C.py
@@ -0,0 +1,5 @@
+variavel=int(input("digite um numero"))
+nome=int(input("digite mais um numero"))
+numero=int(input("digite outro numero"))
+media=(variavel+nome+numero)/3
+print(media)
